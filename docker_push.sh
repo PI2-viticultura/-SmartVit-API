5490cb2ccd6d94912ebf59ec9475cdbc4dd273d3
Luciano Santos <luciano_z7@hotmail.com>
2020-10-05 22:26:03 -0300

    [Test] Testing commands

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag feedback-service lucianosz7/smartvit-feedback
+docker tag smartvit-feedback_feedback-service lucianosz7/smartvit-feedback
 docker push lucianosz7/smartvit-feedback
